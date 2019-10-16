91b4f1b8120edd6639f5667b67a1c1419898e002
Prabhat <prabhat@vinsol.com>
2019-10-16 18:34:37 +0530

    Intial commit files added with gitignore

---
+++ GitExeWork/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitExeWork
+//
+//  Created by Prabhat on 16/10/19.
+//  Copyright © 2019 Prabhat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
